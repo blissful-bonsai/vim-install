e7c203213951254e33f8cbdae63b56e9ae45523e
Tiago <tiagozf2@gmail.com>
2023-08-22 19:17:46 -0300

    First version. Script allows the user to choose between dracula and gruvbox, creates basic ~/.vimrc file with indendation and syntax configuration

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vim Installation Script
+
+I created this script to configure Vim in a new system. The configuration is simple, and you can choose from two color themes: Gruvbox and Dracula.
+
+## Planned Improvements
+1. Support more package managers (Zypper, snap, flatpak), and also allow the user to choose which package manager they wish to use.
+2. Make the script more dynamic, allowing the user to specify things like tabstop, shiftwidth, and themes.
+
+## Usage
+1. Clone or download this repository to your system.
+2. Open a terminal and navigate to the script's location.
+3. Run the script using the following command: `./script.sh`
+
+## Contribution
+
+Your feedback and contributions are welcome! Feel free to open issues and pull requests to help improve this script.
+
+## License
+This project is licensed under the terms of the [MIT License](LICENSE).
---
+++ installation.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+echo -e "\n"
+echo -e "This script is intended for macOS and Linux distributions that use the apt, dnf, or pacman package managers.\n"
+echo -e "On macOS, git and vim are installed by default.\n"
+echo "Press any key to continue"
+read -n 1 -s
+
+function installation(){
+    packages="vim git"
+    if [ -x "$(command -v apt)" ]; then
+        sudo apt update && sudo apt install $packages
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf update && sudo dnf install $packages
+    elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman -Syu $packages
+    else
+        echo "It seems like this script does not support your distro or operating system."
+        exit 1
+    fi
+}
+
+installation
+
+# General configuration
+echo "set tabstop=4" >> ~/.vimrc
+echo "set shiftwidth=4" >> ~/.vimrc
+echo "set expandtab" >> ~/.vimrc
+echo "set number" >> ~/.vimrc
+echo "syntax enable" >> ~/.vimrc
+
+# Color selection
+echo -e "Please, select a color theme\n0 - Dracula\n1 - Gruvbox."
+
+while true; do
+    read -p "Enter your choice: " user_choice
+    
+    if [[ "$user_choice" =~ ^[0-1]$ ]]; then
+        break
+    else
+        echo "Invalid choice. Please enter 0 or 1."
+    fi
+done
+
+if [ "$user_choice" -eq 0 ]; then
+    chosen_theme="dracula"
+    if [ ! -d ~/.vim/pack/themes/start/dracula ]; then
+        mkdir -p ~/.vim/pack/themes/start
+        cd ~/.vim/pack/themes/start
+        git clone https://github.com/dracula/vim.git dracula
+    fi
+
+elif [ "$user_choice" -eq 1 ]; then
+    chosen_theme="gruvbox"
+    if [ ! -d ~/.vim/pack/themes/start/gruvbox ]; then
+        echo "Please keep in mind that the Gruvbox theme offers both dark and light themes, along with contrast options for each theme variant. Refer to their GitHub page for more information."
+        echo "Press any key to continue"
+        read -n 1 -s
+        mkdir -p ~/.vim/pack/themes/start
+        cd ~/.vim/pack/themes/start
+        git clone https://github.com/morhetz/gruvbox.git gruvbox
+    fi
+fi
+
+echo "packadd! $chosen_theme" >> ~/.vimrc
+echo "colorscheme $chosen_theme" >> ~/.vimrc
+echo "$chosen_theme is the default Vim theme now!"
